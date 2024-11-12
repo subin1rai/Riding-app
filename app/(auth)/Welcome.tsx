5656b1eac76a6f441e84395a63d6963e68ead08d
MR. Subi <bantawasubin@gmail.com>
2024-11-12 23:32:18 +0545

    skip section in welcome page

--- app/(auth)/Welcome.tsx
+++ app/(auth)/Welcome.tsx
@@ -6,10 +6,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const OnBoarding = () => {
     return (
         <SafeAreaView className="flex h-full items-center justify-between bg-white">
-            <TouchableOpacity onPress={() => {
-                router.replace("/(auth)/sign-up"); // Pass the URL as a string
-            }}>
-                <Text>Skip</Text>
+            <TouchableOpacity 
+            onPress={() => {
+                router.replace("/(auth)/sign-up"); 
+            }}
+            className="w-full flex justify-end items-end p-12"
+            > 
+                <Text className="text-red-400 text-4xl">Skip</Text>
             </TouchableOpacity>
         </SafeAreaView>
     )
