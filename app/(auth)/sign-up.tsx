f57f589382420e56bef544bc3b938d91fca42da6
MR. Subi <bantawasubin@gmail.com>
2024-11-19 21:22:50 +0545

    verification

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,14 +2,60 @@ import CustomButton from "@/components/CustomButtons";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/Oauth";
 import { icons, images } from "@/constants";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { useState } from "react";
 import { Image, ScrollView, Text, View } from "react-native";
-
+import { useSignUp } from "@clerk/clerk-expo";
+import * as React from "react";
+import {ReactNativeModal} from "react-native-modal";
 const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
   const [form, setForm] = useState({ name: "", email: "", password: "" });
-  const onSignUpPress = async()=>{
+   const [verification, setVerification] = useState({
+    state: 'pending ',
+    error:'',
+    code:''
+   })
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
 
+    try {
+      await signUp.create({
+        emailAddress : form.email,
+        password: form.password,
+      });
+
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      setVerification({...verification,state: 'pending',  });
+    } catch (err: any) {
+     
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  const onPressVerify = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code: verification.code ,
+      });
+
+      if (completeSignUp.status === "complete") {
+        //TODOO : create a database user;
+        await setActive({ session: completeSignUp.createdSessionId });
+        setVerification({...verification,state:'success'});
+      } else {
+        setVerification({...verification,state:'failed', error:"verificatioon failed "});
+      }
+    } catch (err: any) {
+     setVerification({...verification,error:err.error[0].longMessage,state:"failed"});
+    }
   };
   return (
     <ScrollView className="flex-1 bg-white">
@@ -39,20 +85,41 @@ const SignUp = () => {
             label="Password"
             placeholder="Enter your Password"
             icon={icons.lock}
-            secureTextEntry= {true}
+            secureTextEntry={true}
             value={form.password}
             onChangeText={(value) => setForm({ ...form, password: value })}
           />
 
-          <CustomButton title="sign Up" onPress={onSignUpPress} className="mt-6 h-14" />
+          <CustomButton
+            title="sign Up"
+            onPress={onSignUpPress}
+            className="mt-6 h-14"
+          />
 
-          <OAuth/>
-       <Link href={"/sign-in"} className="text-lg text-center text-general-200 mt-10">
-       <Text>Already have an Account?</Text>
-       <Text className="text-primary-500"> Log In</Text>
-       </Link>
+          <OAuth />
+          <Link
+            href={"/sign-in"}
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already have an Account?</Text>
+            <Text className="text-primary-500"> Log In</Text>
+          </Link>
         </View>
         {/* verification model  */}
+        <ReactNativeModal isVisible={verification.state==="success"}>
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+              <Image source={images.check} className="W-[110px] h-[110px] mx-auto my-5" />
+
+              <Text className="text-3xl font-JakartaBold text-center">
+                Verified
+              </Text>
+              
+              <Text className="text-base text-gray-400 font-Jakarta text-center">
+                you have successfully verified
+              </Text>
+              <CustomButton  title="Browse Hme" onPress={()=>router.replace('/(root)/(tabs)/home')} className="mt-5"/>
+          </View>
+        </ReactNativeModal>
       </View>
     </ScrollView>
   );
